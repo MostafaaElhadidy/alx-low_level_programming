65b040cd03d155942cd86cd6f080aed95c5432cc
MostafaaElhadidy <dremyolok2322@gmail.com>
2023-09-07 11:52:05 -0700

    my first c programs yaay

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+*/
+here you can write whatever you want xD
+*/
+
+int main(void){
+printf("Size of a char: %d byte(s)\n",sizeof(char));
+printf("Size of an int: %d byte(s)\n",sizeof(int));
+printf("Size of a long int: %d byte(s)\n",sizeof(long int));
+printf("Size of a longlong int: %d byte(s)\n",sizeof(long long int));
+printf("Size of a float: %d byte(s)\n",sizeof(float));
+
+
+return (0);
+}
