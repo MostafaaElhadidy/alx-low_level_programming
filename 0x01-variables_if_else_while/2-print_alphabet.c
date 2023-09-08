cf4fe60cac1b8a9df137acc01a8322f63e011906
MostafaaElhadidy <dremyolok2322@gmail.com>
2023-09-09 02:59:11 +0300

    added all the files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for(char letter ='a';letter<='z';letter++){
+
+    putchar(letter);
+
+}
+
+	return (0);
+}
